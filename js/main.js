ee00130e66669bf74357d75617af297cb585897d
Utkarsh Sharma <skhiearth@gmail.com>
2020-11-13 00:49:59 +0530

    .

--- js/main.js
+++ js/main.js
@@ -152,4 +152,119 @@
 
 }());
 
+function init () {
+
+	wait(1000).then(() => {
+	  clearText()
+	  typeText('I\'m ').then(typeLoop)
+	})
+	
+	function typeLoop() {
+	  typeText('a, Data Scientist')
+		.then(() => wait(1000))
+		.then(() => removeText('a, Data Scientist'))
+		.then(() => typeText('an, iOS Developer'))
+		.then(() => wait(1000))
+		.then(() => removeText('iOS Developer'))
+		.then(() => typeText('Ethereum Developer'))
+		.then(() => wait(1000))
+		.then(() => removeText('an, Ethereum Developer'))
+		.then(() => typeText('a, ML/AI Enthusiast'))
+		.then(() => wait(1000))
+		.then(() => removeText('a, ML/AI Enthusiast'))
+		.then(() => typeText('a, Financial Tech Enthusiast'))
+		.then(() => wait(1000))
+		.then(() => removeText('a, Finance and Business admirer'))
+		.then(typeLoop)
+	}
+	
+  }
+  
+  
+  // Source code 🚩
+  
+  const elementNode = document.getElementById('type-text')
+  let text = ''
+  
+  function updateNode () {
+	elementNode.innerText = text
+  }
+  
+  function pushCharacter (character) {
+	text += character
+	updateNode()
+  }
+  
+  function popCharacter () {
+	text = text.slice(0, text.length -1)
+	updateNode()
+  }
+  
+  function clearText () {
+	text = ''
+	updateNode()
+  }
+  
+  
+  function wait (time) {
+	if (time === undefined) {
+	  const randomMsInterval = 100 * Math.random()
+	  time = randomMsInterval < 50 ? 10 : randomMsInterval
+	}
+	
+	return new Promise(resolve => {
+	  setTimeout(() => {
+		requestAnimationFrame(resolve)
+	  }, time)
+	})
+  }
+  
+  function typeCharacter (character) {
+	return new Promise(resolve => {
+	  pushCharacter(character)
+	  wait().then(resolve)
+	})
+  }
+  
+  function removeCharacter () {
+	return new Promise(resolve => {
+	  popCharacter()
+	  wait().then(resolve)
+	})
+  }
+  
+  function typeText (text) {
+	return new Promise(resolve => {
+	  
+	  function type ([ character, ...text ]) {
+		typeCharacter(character)
+		  .then(() => {
+			if (text.length) type(text)
+			else resolve()
+		  })
+	  }
+	  
+	  type(text)
+	})
+  }
+  
+  function removeText ({ length:amount }) {
+	return new Promise(resolve => {
+	  
+	  function remove (count) {
+		removeCharacter()
+		  .then(() => {
+			if (count > 1) remove(count - 1)
+			else resolve()
+		  })
+	  }
+	  
+	  remove(amount)
+	})
+  }
+  
+  
+  init()
+  
+
 particlesJS("particles-js", {"particles":{"number":{"value":90,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":400,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
